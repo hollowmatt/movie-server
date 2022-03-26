740d9e8150097ee393e4c77eb66b72c28ca910b5
Matt Holloway <hollowmatt@gmail.com>
2022-03-26 15:52:46 -0400

    add getMovies method

--- controllers/movie-ctrl.js
+++ controllers/movie-ctrl.js
@@ -117,7 +117,24 @@ getMovieById = async(req, res) => {
 }
 
 getMovies = async(req, res) => {
-
+    await Movie.find({}, (err, movie) => {
+        if(err) {
+            return res.status(400).json({
+                success: false,
+                error: err,
+            });
+        }
+        if(!this.getMovies.length) {
+            return res.status(404).json({
+                success: false,
+                error: 'Movie not found',
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            data: this.getMovies,
+        });
+    }).catch(err => console.log(err));
 }
 
 module.exports = {
