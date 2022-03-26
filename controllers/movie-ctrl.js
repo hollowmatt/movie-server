fe14f2fe6dfba5177fe3c78510b0e9ea11defec3
Matt Holloway <hollowmatt@gmail.com>
2022-03-26 15:49:15 -0400

    add getMovieById method

--- controllers/movie-ctrl.js
+++ controllers/movie-ctrl.js
@@ -96,7 +96,24 @@ deleteMovie = async(req, res) => {
 }
 
 getMovieById = async(req, res) => {
-
+    await Movie.findOne({_id: req.params.id}, (err, movie) => {
+        if(err) {
+            return res.status(400).json({
+                success: false,
+                error: err,
+            });
+        }
+        if(!movie) {
+            return res.status(404).json({
+                success: false,
+                error: 'Movie not found',
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            data: movie,
+        });
+    }).catch(err => console.log(err));
 }
 
 getMovies = async(req, res) => {
